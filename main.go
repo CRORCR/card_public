c4668eceea58004b357dda59717f1c5a42afb247
dongshijie <dongshijie@yoawo.com>
2018-09-06 15:33:09 +0800

    add redis server

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"./server/db"
+	"./server/ndb"
 	"./server/mainserver"
 	"fmt"
 )
@@ -12,5 +13,9 @@ func main() {
 		panic("数据库打开失败...")
 	}
 	fmt.Println("数据库打开成功...")
+	err := ndb.Init("./config/redis.json")
+	if nil != err {
+		panic("Redis Error:",err )
+	}
 	rpcserver.Start()
 }
---
+++ server/ndb/redis.go
@@ -0,0 +1,54 @@
+package ndb
+
+import (
+	"os"
+	"fmt"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/astaxie/beego/cache"
+	"github.com/astaxie/beego/cache/redis"
+)
+
+type RedisConf struct {
+	Host string `json:"redisHost"` //
+	User string `json:"redisUser"` //
+	Pwd  string `json:"redisPwd"`  //
+	Port string `json:"redisPort"` //
+	Rndb cache.Cache
+}
+
+var Rndb RedisConf
+
+func GetRndbHand()*RedisConf{
+	return Rndb
+}
+
+func (this *RedisConf)Init(configFile string) {
+	jsonFile, err := os.Open(configFile)
+	defer jsonFile.Close()
+	if (err != nil) {
+		panic("配置文件读取错误")
+	}
+	jsonData, err := ioutil.ReadAll(jsonFile)
+	if (err != nil) {
+		panic("配置文件读取失败")
+	}
+	json.Unmarshal(jsonData, &this)
+}
+
+func (this *RedisConf)GetRedis(configPath string) error {
+	this.Init(configPath)
+	var s = fmt.Sprintf("{\"key\":\"redis\",\"conn\":\"%s:%s\",\"dbNum\":\"%d\",\"password\":\"%s\"}",
+				this.Host,
+				this.Port,
+				0,
+				this.Paw)
+	fmt.Println("Redis Info:", s )
+	this.Rndb = redis.NewRedisCache()
+	return this.Rndb.StartAndGC(s)
+}
+
+//func main() {
+//	redis, _ := getRedis()
+//	redis.Put("test", 100, time.Second*200)
+//}
