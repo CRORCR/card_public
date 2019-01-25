4881cfeff7e15103d92bd551117515c459103832
CRORCR <xingzhiheyi_code@163.com>
2019-01-25 17:27:18 +0800

    add .

--- card_public.go
+++ card_public.go
@@ -14,12 +14,17 @@ func main() {
 		panic("数据库打开失败...")
 	}
 	var redis db.RedisServer
-        if err := redis.Start("./config/redis.json"); nil != err {
-                fmt.Println("Redis Error: ", err)
-                return
-        }
+	if err := redis.Start("./config/redis.json"); nil != err {
+		fmt.Println("Redis Error: ", err)
+		return
+	}
+	if err := modes.InviteInit("./config/output.txt"); nil != err {
+		fmt.Println("File Error:", err)
+		return
+	}
 	fmt.Println("REDIS")
 	fmt.Println("数据库打开成功...")
 	fmt.Println("启动RPC服务")
 	rpcserver.Start()
 }
+
