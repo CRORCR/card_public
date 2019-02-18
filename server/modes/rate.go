a94c0f6b36168abe89025ef43eb56017c304ca72
CRORCR <xingzhiheyi_code@163.com>
2019-02-18 17:37:49 +0800

    利率模块完成

--- server/modes/rate.go
+++ server/modes/rate.go
@@ -1,46 +1,45 @@
-
 package modes
 
 import (
 	"public/server/db"
-	"errors"
 )
 
 const YOAWORATE = "yoawo_rate"
+
 /**
  * 行业以及费率模型
  */
 type YoawoRate struct {
-	Id      int64   `xorm:"id"`
-	ClasId  int64   `xorm:"class_id"`
-	LevelId int64   `xorm:"level_id"`
-	Name    string  `xorm:"name"`
-	Rate1   float64 `xorm:"rate_1"`
-	Rate2   float64 `xorm:"rate_2"`
-	Rate3   float64 `xorm:"rate_3"`
-	Rate4   float64 `xorm:"rate_4"`
-	Rate5   float64 `xorm:"rate_5"`
-	Rate6   float64 `xorm:"rate_6"`
-	Rate7   float64 `xorm:"rate_7"`
+	Id      int64   `json:"id "xorm:"id"`   //表    ID
+	ClasId  int64   `json:"class_id "xorm:"class_id"` //一 类 ID
+	LevelId int64   `json:"level_id "xorm:"level_id"` //二 类 ID
+	Name    string  `json:"name "xorm:"name"`  //名 称 ID
+	Rate1   float64 `json:"rate_1 "xorm:"rate_1"`//费    率
+	Rate2   float64 `json:"rate_2 "xorm:"rate_2"`//费    率
+	Rate3   float64 `json:"rate_3 "xorm:"rate_3"`//费    率
+	Rate4   float64 `json:"rate_4 "xorm:"rate_4"`//费    率
+	Rate5   float64 `json:"rate_5 "xorm:"rate_5"`//费    率
+	Rate6   float64 `json:"rate_6 "xorm:"rate_6"`//费    率
+	Rate7   float64 `json:"rate_7 "xorm:"rate_7"`//费    率
 }
 
 func (this *YoawoRate)GetOne( inPara, outPara *YoawoRate )error{
-	_, e := db.GetDBHand(0).Table( YOAWORATE ).Get( inPara )
+	_, e := db.GetDBHand(0).Table( YOAWORATE ).Get( outPara )
 	return e
 }
 
 /**
 获取当前分类下的子类信息
 */
-func (this YoawoRate)GetList( inPara *YoawoRate, outPara *[]YoawoRate )error{
-	e := db.GetDBHand(0).Table( YOAWORATE ).Where("class_id=?", this.ClasId).Find( outPara )
+func (this *YoawoRate)GetList( inPara *YoawoRate, outPara *[]YoawoRate )error{
+	e := db.GetDBHand(0).Table( YOAWORATE ).Where("class_id = ?", inPara.ClasId).Find( outPara )
 	return e
 }
 
 /**
 获取当前分类下的子类信息
 */
-func (this YoawoRate)GetTopList( inPara *YoawoRate, outPara *[]YoawoRate )error{
+func (this *YoawoRate)GetTopList( inPara *YoawoRate, outPara *[]YoawoRate )error{
 	e := db.GetDBHand(0).Table( YOAWORATE ).Where("level_id= 0 ").Find( outPara )
 	return e
 }
@@ -48,38 +47,17 @@ func (this YoawoRate)GetTopList( inPara *YoawoRate, outPara *[]YoawoRate )error{
 /**
 根据名称搜索
 */
-func (this YoawoRate) GetListByName( inPara *YoawoRate, outPara *[]YoawoRate )error{
-	e := db.GetDBHand(0).SQL("SELECT * FROM " + YOAWORATE + " WHERE name like '%" + this.Name + "%' ").Find(outPara)
+func (this *YoawoRate) GetListByName( inPara *YoawoRate, outPara *[]YoawoRate )error{
+	e := db.GetDBHand(0).SQL("SELECT * FROM " + YOAWORATE + " WHERE name like '%" + inPara.Name + "%' ").Find(outPara)
 	return e
 }
 
 /*
   更新数据
 */
-func (this YoawoRate)U( inPara, outPara *YoawoRate )error{
-	var err error
-	if inPara.Rate1 > 6 &&
-	   inPara.Rate2 > 6 &&
-	   inPara.Rate3 > 6 &&
-	   inPara.Rate4 > 6 &&
-	   inPara.Rate5 > 6 &&
-	   inPara.Rate6 > 6 &&
-	   inPara.Rate7 > 6 {
-	   _, err = db.GetDBHand(0).Where("class_id = ? AND level_id = ?", inPara.ClasId, inPara.LevelId ).
-			        Cols("rate_1").
-			        Cols("rate_2").
-			        Cols("rate_3").
-			        Cols("rate_4").
-			        Cols("rate_5").
-			        Cols("rate_6").
-			        Cols("rate_7").
-				Update(inPara)
-	}else{
-		return errors.New("请查看费率，不可以低于6")
-	}
+func (this *YoawoRate)Update( inPara, outPara *YoawoRate )error{
+	_, err := db.GetDBHand(0).Table( YOAWORATE ).
+				  Where("id = ?", inPara.Id ).
+				  Update(inPara)
         return err
 }
-
-
-
-
---
+++ test/rate_test.go
@@ -0,0 +1,120 @@
+package test
+
+import (
+	"fmt"
+	"net/rpc"
+	"public/server/modes"
+	"testing"
+)
+
+/**
+ * @desc    TODO
+ * @author Ipencil
+ * @create 2019/2/18
+ */
+
+var rateList []*modes.YoawoRate
+
+
+func TestRate(t *testing.T) {
+	getTopList()
+	//getList()
+	//getListByName()
+	//getRateBy()
+	//update()
+}
+
+/* 获得所有 level_id= 0 行业,拿到行业classid,根据classid去查询所有的数据*/
+func getTopList() error {
+	client, err := rpc.Dial("tcp", "127.0.0.1:7003")
+	defer func() { client.Close() }()
+	if err != nil {
+		fmt.Println("连接RPC服务失败:", err)
+	}
+	fmt.Println("连接RPC服务成功")
+
+	this:= &modes.YoawoRate{}
+	err = client.Call("YoawoRate.GetTopList", this, &rateList)
+	if err != nil {
+		fmt.Println("调用失败:", err)
+	}
+	fmt.Println(len(rateList))
+	fmt.Printf("%+v\n",rateList)
+	return err
+}
+
+/*根据classid去查询所有的数据*/
+func getList() error {
+	client, err := rpc.Dial("tcp", "127.0.0.1:7003")
+	defer func() { client.Close() }()
+	if err != nil {
+		fmt.Println("连接RPC服务失败:", err)
+	}
+	fmt.Println("连接RPC服务成功")
+
+	this:= &modes.YoawoRate{ClasId:1}
+	err = client.Call("YoawoRate.GetList", this, &rateList)
+	if err != nil {
+		fmt.Println("调用失败:", err)
+	}
+	fmt.Println(len(rateList))
+	fmt.Printf("%+v\n",rateList)
+	return err
+}
+
+/*根据name模糊查询*/
+func getListByName() error {
+	client, err := rpc.Dial("tcp", "127.0.0.1:7003")
+	defer func() { client.Close() }()
+	if err != nil {
+		fmt.Println("连接RPC服务失败:", err)
+	}
+	fmt.Println("连接RPC服务成功")
+
+	this:= &modes.YoawoRate{Name:"中"}
+	err = client.Call("YoawoRate.GetListByName", this, &rateList)
+	if err != nil {
+		fmt.Println("调用失败:", err)
+	}
+	fmt.Println(len(rateList))
+	fmt.Printf("%+v\n",rateList)
+	return err
+}
+
+/*获得一条数据*/
+func getRateBy() error {
+	client, err := rpc.Dial("tcp", "127.0.0.1:7003")
+	defer func() { client.Close() }()
+	if err != nil {
+		fmt.Println("连接RPC服务失败:", err)
+	}
+	fmt.Println("连接RPC服务成功")
+
+	this:= &modes.YoawoRate{Id:2}
+	err = client.Call("YoawoRate.GetOne", this, this)
+	if err != nil {
+		fmt.Println("调用失败:", err)
+	}
+	fmt.Println(len(rateList))
+	fmt.Printf("%+v\n",this)
+	return err
+}
+
+/*更新数据*/
+func update() error {
+	client, err := rpc.Dial("tcp", "127.0.0.1:7003")
+	defer func() { client.Close() }()
+	if err != nil {
+		fmt.Println("连接RPC服务失败:", err)
+	}
+	fmt.Println("连接RPC服务成功")
+
+	this:= &modes.YoawoRate{Id:2,Rate1:100}
+	err = client.Call("YoawoRate.Update", this, this)
+	if err != nil {
+		fmt.Println("调用失败:", err)
+	}
+	fmt.Println(len(rateList))
+	fmt.Printf("%+v\n",this)
+	return err
+}
